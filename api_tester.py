3726e6c787947da4f4ba518544a88d07b8bc4cfd
awcrusius <awcrusius@gmail.com>
2024-11-03 18:27:19 -0800

    initial commit

--- api_tester.py
+++ api_tester.py
@@ -7,21 +7,15 @@ Provider = 'Translink' # use to change data provider
 #create full url from auth.yaml
 with open('.config/my_api_keys/auth.yaml', 'r') as file:
     auth = yaml.load(file,Loader=yaml.SafeLoader)     
-url = auth[Provider]['link'] + auth[Provider]['api_key']
+url = auth[Provider]['position_link'] + auth[Provider]['api_key']
 
 #create gtfs feed
 feed = gtfs_realtime_pb2.FeedMessage()
 
-#create full link to api including api key 
-
-#print(url)
-
-
 response = requests.get(url)
 if response.status_code == 200:
-    #data = response.json() #change to protobuf
-
-    print(response.content)
+    feed.ParseFromString(response.content)
+    print(feed.entity[1])
 
 
 #print(response)
